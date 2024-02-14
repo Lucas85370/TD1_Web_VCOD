1eb36cbdd24fb44f04fe89b1d2484eecf0d51bcb
Lucas85370 <lucas.sicard@etu.univ-poitiers.fr>
2024-02-14 09:05:13 +0100

    I added the javascript folder with 1 file and update the file on the other folders

---
+++ javascript/ville.js
@@ -0,0 +1,20 @@
+//Code javascript
+
+function afficheVille() {
+    // 1. Récupération de la valeur courante du select avec son id
+    let nomVilleChoisie = document.getElementById("choixVille").value;
+    // 2. Récupération de la liste de toutes les div correspondant à une div de météo
+    // grâce à la classe « ville »
+    let villes = document.getElementsByClassName("ville");
+    // 3. Parcours de la liste des div météo. On cache celles non sélectionnées.
+    for (let i = 0; i < choixVille.length; i++) {
+    // l’attribut id de l’élément est comparée à la ville choisie
+        if (villes[i].id === nomVilleChoisie) {
+        // garder l'élément visible -> ELEMENT.style.display= "";
+            villes[i].style.display="block";
+        } else {
+        // cacher l'élément -> ELEMENT.style.display= "none";
+            villes[i].style.display="none";
+        }
+    }
+}
